e4e02ddb1c3fb02a737cf36e884a77c75d545be9
Anita <z5479318@ad.unsw.edu.au>
2023-09-19 21:10:43 +1000

    Added a stub function for adminQuizInfo to the quiz.js file

--- src/quiz.js
+++ src/quiz.js
@@ -15,4 +15,19 @@ function adminQuizRemove(authUserId, quizId) {
     return {};
   }
 
+// Stub for adminQuizInfo function
+function adminQuizInfo(authUserId, quizId) {
+    return {
+      quizId: 1,
+      name: 'My Quiz',
+      timeCreated: 1683125870,
+      timeLastEdited: 1683125871,
+      description: 'This is my quiz',
+    };
+  }
+
+
+
+
+
 
