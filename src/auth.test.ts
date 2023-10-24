f2c826b3084cad049ebf00febf04586d017a8796
Anita Byun <z5479318@ad.unsw.edu.au>
2023-10-24 17:16:18 +1100

    added code: modified tests for adminAuthLogin and added tests for adminUserDetail

--- src/auth.test.ts
+++ src/auth.test.ts
@@ -3,6 +3,42 @@ import { port, url } from './config.json';
 import { clear } from './other';
 const SERVER_URL = `${url}:${port}`;
 
+function requestAuthRegister(email: string, password: string, nameFirst: string, nameLast: string) {
+  const res = request(
+    'POST',
+    SERVER_URL + '/v1/admin/auth/register',
+    {
+      json: {
+        email,
+        password,
+        nameFirst,
+        nameLast
+      },
+      timeout: 100
+    }
+  );
+
+  return JSON.parse(res.body.toString());
+}
+
+function requestAuthLogin(email: string, password: string) {
+  const res = request(
+    'POST',
+    SERVER_URL + '/v1/admin/auth/login',
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    }
+  );
+
+  return JSON.parse(res.body.toString());
+}
+
 describe('adminAuthRegister', () => {
   beforeEach(() => {
     request(
@@ -409,5 +445,241 @@ describe('adminAuthLogin', () => {
 
     const data = JSON.parse(loginResponse.body.toString());
     expect(data).toStrictEqual({ error: 'Incorrect password' });
+  });
+});
+
+describe('adminUserDetail', () => {
+  beforeEach(() => {
+    request('DELETE', SERVER_URL + '/v1/clear');
+  });
+
+  test('Return user details for a valid authUserId', () => {
+    const registrationResponse = request(
+      'POST',
+      SERVER_URL + '/v1/admin/auth/register',
+      {
+        json: {
+          email: 'anita@unsw.edu.au',
+          password: 'password123',
+          nameFirst: 'Anita',
+          nameLast: 'Byun',
+        },
+      }
+    );
+
+    const registrationData = JSON.parse(registrationResponse.body.toString());
+    const userId = registrationData.userId;
+    const res = request(
+      'GET',
+      SERVER_URL + '/v1/admin/user/details',
+      {
+        qs: { userId: userId },
+      }
+    );
+    const data = JSON.parse(res.body.toString());
+
+    const expectedUserDetails = {
+      user: {
+        email: 'anita@unsw.edu.au',
+        name: 'Anita Byun',
+        numFailedPasswordsSinceLastLogin: 0,
+        numSuccessfulLogins: 1,
+        userId: 0,
+      }
+    };
+
+    expect(data).toEqual(expectedUserDetails);
+  });
+
+  test('Return an error for an invalid authUserId', () => {
+    const registrationResponse = request(
+      'POST',
+      SERVER_URL + '/v1/admin/auth/register',
+      {
+        json: {
+          email: 'anita@unsw.edu.au',
+          password: 'password123',
+          nameFirst: 'Anita',
+          nameLast: 'Byun',
+        },
+      }
+    );
+
+    const registrationData = JSON.parse(registrationResponse.body.toString());
+    const userId = registrationData.userId;
+
+    const res = request(
+      'GET',
+      SERVER_URL + '/v1/admin/user/details',
+      {
+        qs: { userId: userId + 1 },
+      }
+    );
+    const data = JSON.parse(res.body.toString());
+    expect(data).toEqual({ error: 'Invalid authUserId' });
+  });
+
+  test('Test two successful logins', () => {
+    const registrationResponse = request(
+      'POST',
+      SERVER_URL + '/v1/admin/auth/register',
+      {
+        json: {
+          email: 'william@unsw.edu.au',
+          password: '1234abcd',
+          nameFirst: 'William',
+          nameLast: 'Lu',
+        },
+      }
+    );
+
+    const registrationData = JSON.parse(registrationResponse.body.toString());
+    const userId = registrationData.userId;
+    expect(userId).toEqual(expect.any(Number));
+
+    const loginResponse = request(
+      'POST',
+      SERVER_URL + '/v1/admin/auth/login',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: 'william@unsw.edu.au',
+          password: '1234abcd',
+        }),
+      }
+    );
+    const returnedUserId = JSON.parse(loginResponse.body.toString()).userId;
+    expect(returnedUserId).toEqual(userId);
+    const res = request(
+      'GET',
+      SERVER_URL + '/v1/admin/user/details',
+      {
+        qs: { userId: userId },
+      }
+    );
+    const data = JSON.parse(res.body.toString());
+
+    const expectedUserDetails = {
+      user: {
+        email: 'william@unsw.edu.au',
+        name: 'William Lu',
+        numFailedPasswordsSinceLastLogin: 0,
+        numSuccessfulLogins: 2,
+        userId: userId,
+      }
+    };
+
+    expect(data).toEqual(expectedUserDetails);
+  });
+
+  test('Test one unsuccessful login', () => {
+    const registrationResponse = request(
+      'POST',
+      SERVER_URL + '/v1/admin/auth/register',
+      {
+        json: {
+          email: 'william@unsw.edu.au',
+          password: '1234abcd',
+          nameFirst: 'William',
+          nameLast: 'Lu',
+        },
+      }
+    );
+
+    const registrationData = JSON.parse(registrationResponse.body.toString());
+    const userId = registrationData.userId;
+    expect(userId).toEqual(expect.any(Number));
+
+    const loginResponse = request(
+      'POST',
+      SERVER_URL + '/v1/admin/auth/login',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: 'william@unsw.edu.au',
+          password: '1234abcde',
+        }),
+      }
+    );
+    const errorResponse = JSON.parse(loginResponse.body.toString());
+    expect(errorResponse).toStrictEqual({ error: 'Incorrect password' });
+    const res = request(
+      'GET',
+      SERVER_URL + '/v1/admin/user/details',
+      {
+        qs: { userId: userId },
+      }
+    );
+    const data = JSON.parse(res.body.toString());
+
+    const expectedUserDetails = {
+      user: {
+        email: 'william@unsw.edu.au',
+        name: 'William Lu',
+        numFailedPasswordsSinceLastLogin: 1,
+        numSuccessfulLogins: 1,
+        userId: userId,
+      }
+    };
+
+    expect(data).toEqual(expectedUserDetails);
+  });
+
+  test('Test unsuccessful login reset on success', () => {
+    const registrationData = requestAuthRegister('william@unsw.edu.au', '1234abcd', 'William', 'Lu');
+    const userId = registrationData.userId;
+    expect(userId).toEqual(expect.any(Number));
+
+    const errorResponse = requestAuthLogin('william@unsw.edu.au', '1234abcde');
+    expect(errorResponse).toStrictEqual({ error: 'Incorrect password' });
+
+    const res1 = request(
+      'GET',
+      SERVER_URL + '/v1/admin/user/details',
+      {
+        qs: { userId: userId },
+      }
+    );
+    const data1 = JSON.parse(res1.body.toString());
+
+    const expectedUserDetails1 = {
+      user: {
+        email: 'william@unsw.edu.au',
+        name: 'William Lu',
+        numFailedPasswordsSinceLastLogin: 1,
+        numSuccessfulLogins: 1,
+        userId: userId,
+      }
+    };
+
+    expect(data1).toEqual(expectedUserDetails1);
+
+    const returnedUserId = requestAuthLogin('william@unsw.edu.au', '1234abcd').userId;
+    expect(returnedUserId).toEqual(userId);
+
+    const res2 = request(
+      'GET',
+      SERVER_URL + '/v1/admin/user/details',
+      {
+        qs: { userId: userId },
+      }
+    );
+    const data2 = JSON.parse(res2.body.toString());
+
+    const expectedUserDetails2 = {
+      user: {
+        email: 'william@unsw.edu.au',
+        name: 'William Lu',
+        numFailedPasswordsSinceLastLogin: 0,
+        numSuccessfulLogins: 2,
+        userId: userId,
+      }
+    };
+
+    expect(data2).toEqual(expectedUserDetails2);
   });
 });
